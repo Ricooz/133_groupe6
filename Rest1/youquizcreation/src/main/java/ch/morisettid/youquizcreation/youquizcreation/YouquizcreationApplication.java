b7cf9be877372029284bf4b8e8680c7579afce49
Dadamomo <Dadamo0305@gmail.com>
2024-04-19 12:52:38 +0200

    Rest1 initialise

---
+++ Rest1/youquizcreation/src/main/java/ch/morisettid/youquizcreation/youquizcreation/YouquizcreationApplication.java
@@ -0,0 +1,14 @@
+package ch.morisettid.youquizcreation.youquizcreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouquizcreationApplication {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouquizcreationApplication.class, args);
+	}
+
+}
---
+++ Rest1/youquizcreation/src/test/java/ch/morisettid/youquizcreation/youquizcreation/YouquizcreationApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.morisettid.youquizcreation.youquizcreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouquizcreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
