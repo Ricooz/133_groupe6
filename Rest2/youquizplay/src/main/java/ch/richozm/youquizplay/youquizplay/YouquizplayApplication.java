53ef909e5c68278b2d4f793f4b0abb53af5133cf
Ricooz <matteo.richoz@gmail.com>
2024-04-19 12:55:04 +0200

    rest2 initialise

---
+++ Rest2/youquizplay/src/main/java/ch/richozm/youquizplay/youquizplay/YouquizplayApplication.java
@@ -0,0 +1,13 @@
+package ch.richozm.youquizplay.youquizplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouquizplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouquizplayApplication.class, args);
+	}
+
+}
