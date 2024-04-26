656117a63f9d8711201b1cb423aee400a4e2a673
Dadamomo <Dadamo0305@gmail.com>
2024-04-26 08:28:15 +0200

    API gateway initialisée

---
+++ ApiGateway/src/main/java/ch/emf/youquiz/youquizapi/YouquizapiApplication.java
@@ -0,0 +1,13 @@
+package ch.emf.youquiz.youquizapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouquizapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouquizapiApplication.class, args);
+	}
+
+}
---
+++ ApiGateway/src/test/java/ch/emf/youquiz/youquizapi/YouquizapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.emf.youquiz.youquizapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouquizapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
