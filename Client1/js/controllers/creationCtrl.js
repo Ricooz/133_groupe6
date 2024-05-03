eb4dd5ac8eef85535e9066a822970067c9aa1b22
Dadamomo <Dadamo0305@gmail.com>
2024-05-03 16:41:44 +0200

    Client 1 avancé

--- Client1/js/controllers/creationCtrl.js
+++ Client1/js/controllers/creationCtrl.js
@@ -25,20 +25,25 @@ class CreationCtrl {
         <div id="questionsContainer" class="border-2 border-zinc-700 bg-zinc-900 rounded-xl flex flex-col w-full p-3">
             
         </div>
+    </div>
+    <div class="w-full flex flex-row justify-center my-12 h-20 items-around">
+        <button type="button" class="bg-green-700 hover:bg-green-600 text-white text-md font-bold rounded-xl mx-10 w-1/6 buttonSave"><i class="fa-solid fa-floppy-disk" style="color: #ffffff;"></i></button>
+        <button type="button" class="bg-red-800 hover:bg-red-700 text-white text-md font-bold rounded-xl mx-10 w-1/6 buttonCancel"><i class="fa-solid fa-x" style="color: #ffffff;"></i></button>
     </div>`;
     this.nouvelleQuestionHTML = `<div class="w-full border-2 border-zinc-700 bg-zinc-800 rounded-xl flex flex-col w-full p-2 my-2 question">
       <div class="flex flex-row justify-between items-center mb-4">
           <textarea class="w-full text-gray-300 p-2 border-2 border-zinc-700 bg-zinc-900 rounded-xl questionTitre" rows="1" placeholder="question"></textarea>
           <button type="button" class="bg-green-700 hover:bg-green-600 text-white text-md font-bold rounded-xl my-2 ml-2 px-4 h-10 buttonNouvelleReponse"><i class="fa-solid fa-plus" style="color: #ffffff;"></i></button>
-          <button type="button" class="bg-red-800 hover:bg-red-700 text-white text-md font-bold rounded-xl my-2 ml-2 px-4 h-10 buttonDeleteReponse"><i class="fa-solid fa-trash" style="color: #ffffff;"></i></button>
+          <button type="button" class="bg-red-800 hover:bg-red-700 text-white text-md font-bold rounded-xl my-2 ml-2 px-4 h-10 buttonDeleteQuestion"><i class="fa-solid fa-trash" style="color: #ffffff;"></i></button>
       </div>
-    </div>`; // <button type="button" class="bg-blue-700 text-white text-md font-bold rounded-xl ml-2 px-3 h-9 buttonCorrecte"><i class="fa-solid fa-check" style="color: #ffffff;"></i></button>
+    </div>`;
     this.nouvelleReponseHTML = `<div class="flex flex-row items-center reponse">
       <textarea class="w-full text-gray-300 p-1 border-2 border-zinc-700 bg-zinc-900 rounded-xl my-1" rows="1" placeholder="reponse"></textarea>
       <button type="button" class="bg-zinc-800 border-2 border-blue-700 text-white text-md font-bold rounded-xl ml-2 px-3 h-9 buttonCorrecte"><i class="fa-solid fa-check" style="color: #27272a;"></i></button>
-      <button type="button" class="bg-blue-700 border-2 border-blue-700 text-white text-md font-bold rounded-xl ml-2 pr-3 pl-3 h-9 buttonCorrecte"><i class="fa-solid fa-check" style="color: #ffffff;"></i></button>
       <button type="button" class="bg-red-800 hover:bg-red-700 text-white text-md font-bold rounded-xl ml-2 px-3 h-9 buttonDeleteReponse"><i class="fa-solid fa-trash" style="color: #ffffff;"></i></button>
     </div>`;
+    this.buttonChecked = `<button type="button" class="bg-blue-700 border-2 border-blue-700 text-white text-md font-bold rounded-xl ml-2 pr-3 pl-3 h-9 buttonCorrecte"><i class="fa-solid fa-check" style="color: #ffffff;"></i></button>`;
+    this.buttonUnchecked = `<button type="button" class="bg-zinc-800 border-2 border-blue-700 text-white text-md font-bold rounded-xl ml-2 px-3 h-9 buttonCorrecte"><i class="fa-solid fa-check" style="color: #27272a;"></i></button>`;
   }
 
   estAutorise() {
@@ -68,6 +73,14 @@ class CreationCtrl {
       }
     });
 
+    $(".buttonSave").click((event) => {
+      
+    });
+
+    $(".buttonCancel").click((event) => {
+      this.vueService.changerVue("home");
+    });
+
     $(".buttonNouvelleQuestion").click((event) => {
       let elementQuestion = $(this.nouvelleQuestionHTML).clone();
       $("#questionsContainer").append(elementQuestion);
@@ -75,24 +88,29 @@ class CreationCtrl {
       elementQuestion.find(".buttonNouvelleReponse").click((event) => {
         let elementReponse = $(this.nouvelleReponseHTML).clone();
         elementQuestion.append(elementReponse);
+
+        elementQuestion.on("click", ".buttonCorrecte", (event) => {
+          if ($(event.currentTarget).hasClass("bg-zinc-800")) { // Est unchecked
+            $(event.currentTarget).replaceWith(this.buttonChecked);
+            $(event.currentTarget).data("correcte", true);
+          } else {
+            $(event.currentTarget).replaceWith(this.buttonUnchecked);
+            $(event.currentTarget).data("correcte", false);
+          }
+        });
+
+        elementReponse.find(".buttonDeleteReponse").click((event) => {
+          elementReponse.remove();
+        });
       });
 
       elementQuestion.find(".buttonDeleteQuestion").click((event) => {
-        let elementReponse = $(this.nouvelleReponseHTML).clone();
-        elementQuestion.append(elementReponse);
+        elementQuestion.remove();
       });
     });
     
     if (currentQuiz !== null) {
-      this.loadQuiz(currentQuiz);
+      
     }
-  }
-
-  loadQuiz() {
-
-  }
-
-  save() {
-
   }
 }
